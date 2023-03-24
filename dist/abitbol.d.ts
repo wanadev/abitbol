bfd648dacaac477b6de23ad6f26ac8b211c25c76
Jean-Baptiste Ghoul <jbghoul@wanadev.fr>
2023-03-24 09:55:28 +0100

    dist: generated types declaration

---
+++ dist/abitbol.d.ts
@@ -0,0 +1,13 @@
+export = Class;
+declare function Class(): void;
+declare namespace Class {
+    function $class(): void;
+    namespace $class {
+        namespace $map {
+            const attributes: {};
+            const methods: {};
+            const computedProperties: {};
+        }
+    }
+}
+//# sourceMappingURL=abitbol.d.ts.map
